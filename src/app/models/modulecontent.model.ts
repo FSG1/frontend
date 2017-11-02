5f10f450f7d2cd1217143d29e215ed4b8fabe2e1
nils <n.nieuwenhuis@student.fontys.nl>
2017-11-02 14:07:40 +0100

    added the rest fields for module component, created model for prior knowledge reference

--- src/app/models/modulecontent.model.ts
+++ src/app/models/modulecontent.model.ts
@@ -1,11 +1,25 @@
 import {LearningGoal} from './learninggoal';
 import {ArchitecturalLayer} from './architecturallayer';
 import {LifecycleActivity} from './lifecycleactivity';
+import {StudentSkill} from './studentskill';
+import {PriorKnowledgeReferenceModel} from './prior_knowledge_reference.model';
 
 export interface ModuleContent {
   module_code: string;
   module_name: string;
   credits: number;
+  semester: number;
+  lectures_in_week: number;
+  practical_hours_week: number;
+  total_effort: number;
+  lecturers: string[];
+  crecentials: string;
+  introductorytext: string;
+  qualifications: StudentSkill[];
+  topics: string[];
+  teaching_material: string;
+  prior_knowledge_references: PriorKnowledgeReferenceModel[];
+  additional_information: string;
   architectural_layers: ArchitecturalLayer[];
   lifecycle_activities: LifecycleActivity[];
   learning_goals: LearningGoal[];
---
+++ src/app/models/prior_knowledge_reference.model.ts
@@ -0,0 +1,6 @@
+export interface PriorKnowledgeReferenceModel {
+  module_code: number;
+  module_name: string;
+  type: string;
+  remark: string;
+}
