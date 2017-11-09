161abff349dd25dd766e9287577a235639ab49ef
Tobias Derksen <t.derksen@gmx.de>
2017-11-09 11:39:11 +0100

    Removed unused method

--- src/app/components/semester-overview-component/semester-overview.component.ts
+++ src/app/components/semester-overview-component/semester-overview.component.ts
@@ -1,10 +1,8 @@
 import {AfterContentInit, Component, OnDestroy} from '@angular/core';
 import {CompleteSemester} from '../../models/complete_semester.model';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {BackendService} from '../../backend.service';
 import {Subscription} from 'rxjs/Subscription';
-import {Subscriber} from 'rxjs/Subscriber';
-import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-semester-overview',
@@ -19,7 +17,7 @@ export class SemesterOverviewComponent implements AfterContentInit, OnDestroy {
 
   routeSubscription: Subscription;
 
-  constructor(private backendService: BackendService, private router: Router, private route: ActivatedRoute) {}
+  constructor(private backendService: BackendService, private route: ActivatedRoute) {}
 
   ngAfterContentInit(): void {
     this.routeSubscription = this.route.params.subscribe(
@@ -30,10 +28,6 @@ export class SemesterOverviewComponent implements AfterContentInit, OnDestroy {
           .subscribe(completesemester => this.completesemester = completesemester);
       });
   }
-  onClick(module_code: string): void {
-    this.router.navigate(['/curriculum/', this.curriculumid, 'modules', module_code]);
-  }
-
   ngOnDestroy(): void {
     this.routeSubscription.unsubscribe();
   }
