ed7c6bc03af70d2b5e880b8c3c501f51dbf07ef8
nils <n.nieuwenhuis@student.fontys.nl>
2017-11-13 11:30:44 +0100

    removed unsused import and mocksemester data

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -9,30 +9,6 @@ import 'rxjs/add/operator/map';
 import {CurriculumResponse} from './backend-responses/CurriculumResponse';
 import {ModuleContent} from './models/modulecontent.model';
 import {CompleteSemester} from './models/complete_semester.model';
-import {Subscriber} from 'rxjs/Subscriber';
-
-// Only here until endpoint is implemented
-const mocksemester = {
-  'modules': [
-    {'module_code': 'JAV1',        'module_name': 'Programming in Java 1',       'credits': 5 },
-    {'module_code': 'DBS1',         'module_name': 'Databases',       'credits': 5 },
-    {'module_code': 'BUA',         'module_name': 'Business Administration',       'credits': 4 },
-    {'module_code': 'MAT1',        'module_name': 'Mathematics 1',       'credits': 4 },
-    {'module_code': 'ENG1',        'module_name': 'English 1',       'credits': 1 },
-    {'module_code': 'PRJ1',        'module_name': 'Project 1',       'credits': 10 },
-    {'module_code': 'COM1',        'module_name': 'Communication 1',       'credits': 2 }
-  ],
-  'qualifications': [{'lifecycle_activity': 1, 'architectural_layer': 3, 'level': 1},
-    {'lifecycle_activity': 2, 'architectural_layer': 3, 'level': 2}],
-  'architectural_layers': [{'architectural_layer_id': 0, 'architectural_layer_name': 'User Interaction', 'architectural_layer_description': 'something'},
-    {'architectural_layer_id': 1, 'architectural_layer_name': 'Business Processes', 'Architectural_layer_description': 'something'},
-    {'architectural_layer_id': 2, 'architectural_layer_name': 'Infrastructure', 'architectural_layer_description': 'something'},
-    {'architectural_layer_id': 3, 'architectural_layer_name': 'software', 'architectural_layer_description': 'something'}],
-  'lifecycle_activities': [{'lifecycle_activity_id': 0, 'lifecycle_activity_name': 'Manage', 'lifecycle_activity_description': 'something'},
-    {'lifecycle_activity_id': 1, 'lifecycle_activity_name': 'Analyze', 'lifecycle_activity_description': 'something'},
-    {'lifecycle_activity_id': 2, 'lifecycle_activity_name': 'Advice', 'lifecycle_activity_description': 'something'},
-    {'lifecycle_activity_id': 3, 'lifecycle_activity_name': 'Design', 'lifecycle_activity_description': 'something'}]
-};
 
 // WINDOWS IP:192.168.99.100
 // LINUX IP: 172.17.0.1
