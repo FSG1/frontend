b32b18a7545b58a979daa5a833a1e2ce0c7f7a00
Sjoerd Brauer <s.brauer@student.fontys.nl>
2017-11-23 12:09:29 +0100

    added endpoint with mockdata

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -13,163 +13,69 @@ import {Subscriber} from 'rxjs/Subscriber';
 import {FilterQualifications} from './models/qualificiationfiltermodels/filter_qualifications.model';
 import {QualificationsOverview} from './models/qualificiationfiltermodels/qualifications_overview.model';
 import {environment} from '../environments/environment';
+import {EditableModuleOutput} from './models/editmodels/editable_module_output';
 
-// Only here until endpoint is implemented
-const filterqualifications = {
-  'curricula': [
-    { 'name': 'Software Engineering', 'code': 'SE', 'id': 1},
-    { 'name': 'Business Informatics', 'code': 'BI', 'id': 2}],
-  'lifecycle_activities': [{'lifecycle_activity_id': 0, 'lifecycle_activity_name': 'Manage', 'lifecycle_activity_description': 'something'},
-    {'lifecycle_activity_id': 1, 'lifecycle_activity_name': 'Analyze', 'lifecycle_activity_description': 'something'},
-    {'lifecycle_activity_id': 2, 'lifecycle_activity_name': 'Advice', 'lifecycle_activity_description': 'something'},
-    {'lifecycle_activity_id': 3, 'lifecycle_activity_name': 'Design', 'lifecycle_activity_description': 'something'}],
-  'architectural_layers': [{'architectural_layer_id': 0, 'architectural_layer_name': 'User Interaction', 'architectural_layer_description': 'something'},
-    {'architectural_layer_id': 1, 'architectural_layer_name': 'Business Processes', 'architectural_layer_description': 'something'},
-    {'architectural_layer_id': 2, 'architectural_layer_name': 'Infrastructure', 'architectural_layer_description': 'something'},
-    {'architectural_layer_id': 3, 'architectural_layer_name': 'software', 'architectural_layer_description': 'something'}],
+const outputmockup = {
+  'code': 'DBS',
+  'name': 'Databases',
+  'credits': 5,
+  'semester': 1,
+  'lectures_in_week': 3,
+  'practical_hours_week': 4,
+  'introductorytext': 'very nice module. everyone should follow it',
+  'topics': ['drinking beer', 'sql injection', 'otherstuff'],
+  'teaching_material': [{
+    'name': 'dbs book',
+    'type': 'book'
+  }, {
+    'name': 'dbs book 2',
+    'type': 'book'
+  }, {
+    'name': 'www.something.com',
+    'type': 'website'
+  }],
+  'teaching_material_types': ['book', 'website', 'physical'],
+  'additional_information': 'vey nice course',
+  'all_lecturers': [{
+    'id': 1,
+    'name': 'Dorssers, F',
+  }, {
+    'id': 2,
+    'name': 'Van Odenhoven, F',
+  }, {
+    'id': 5,
+    'name': 'Van der Ham, R',
+  }],
+  'active_lecturers': [{
+    'id': 1,
+    'name': 'Dorssers, F',
+  }, {
+    'id': 2,
+    'name': 'Van Odenhoven, F',
+  }],
+  'credentials': 'vey nice course'
 };
-const mockqualificationtable = [{
-  'skills_level': 1,
-  'qualification_overview_semesters': [
-    {'semester': 1,
-      'qualifications_modules': [
-        {
-          'module_code': 'BUA',
-          'module_name': 'Business informatics',
-          'credits': 4,
-          'learning_goals': [
-            {
-              'name': 'lg1',
-              'description': 'loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsumloremIpsum loremIpsumloremIpsumloremIpsum loremIpsumloremIpsumlorem IpsumloremIpsum loremIpsumloremIpsumloremIpsum'
-            }, {
-              'name': 'lg2',
-              'description': 'blablabla'
-            }]
-        }, {
-          'module_code': 'JAV1',
-          'module_name': 'Java 1',
-          'credits': 5,
-          'learning_goals': [
-            {
-              'name': 'lg3',
-              'description': 'heel mooi'
-            }, {
-              'name': 'lg4',
-              'description': 'blablabla'
-            }, {
-              'name': 'lg5',
-              'description': 'blablabla'
-            }]
-        }, {
-          'module_code': 'DBS',
-          'module_name': 'Databases',
-          'credits': 5,
-          'learning_goals': [
-            {
-              'name': 'lg6',
-              'description': 'blablabla'
-            }]
-        }]
-    },
-    {'semester': 3,
-      'qualifications_modules': [
-        {
-          'module_code': 'BUMA',
-          'module_name': 'Business management',
-          'credits': 5,
-          'learning_goals': [
-            {
-              'name': 'lg7',
-              'description': 'blablabla'
-            }, {
-              'name': 'lg8',
-              'description': 'blablabla'
-            }, {
-              'name': 'lg9',
-              'description': 'blablabla'
-            }]
-        }, {
-          'module_code': 'COM3',
-          'module_name': 'Communicatie 3',
-          'credits': 1,
-          'learning_goals': [
-            {
-              'name': 'lg10',
-              'description': 'blablabla'
-            }, {
-              'name': 'lg11',
-              'description': 'blablabla'
-            }]
-        }]},
-    {'semester': 7,
-      'qualifications_modules': [{
-        'module_code': 'SOFA',
-        'module_name': 'Software factorio',
-        'credits': 15,
-        'learning_goals': [
-          {
-            'name': 'lg4',
-            'description': 'blablabla'
-          }]
-      }]
-    }]},  {
-  'skills_level': 2,
-  'qualification_overview_semesters': [
-    {'semester': 2,
-      'qualifications_modules': [
-        {
-          'module_code': 'etc',
-          'module_name': 'etc',
-          'credits': 15,
-          'learning_goals': [
-            {
-              'name': 'lg4',
-              'description': 'blablabla'
-            }, {
-              'name': 'lg5',
-              'description': 'blablabla'
-            }, {
-              'name': 'lg6',
-              'description': 'blablabla'
-            }]
-        }]
-    },
-    {'semester': 4,
-      'qualifications_modules': [{
-        'module_code': 'etc2',
-        'module_name': 'etc',
-        'credits': 1,
-        'learning_goals': [
-          {
-            'name': 'lg4',
-            'description': 'blablabla'
-          }, {
-            'name': 'lg5',
-            'description': 'blablabla'
-          }, {
-            'name': 'lg6',
-            'description': 'blablabla'
-          }]
-      }
-      ]}]
-}];
 
 @Injectable()
 export class BackendService {
-
   constructor(private http: HttpClient) {
   }
   getSemester(curriculum: number, semester: number): Observable<CompleteSemester> {
     const completeSemesterUrl = this.getBaseUrl() + 'curriculum/' + curriculum + '/semester/' + semester;
     return this.http.get<CompleteSemester>(completeSemesterUrl);
   }
-
-  // endpoint does not exist yet
+  // endpoint doesn't exist yet
+  getModule(modulecode: string): Observable<EditableModuleOutput> {
+   const editablemoduleoutputurl = this.getBaseUrl() + 'module/' + modulecode;
+    return Observable.create((observer: Subscriber<any>) => {
+      observer.next(outputmockup);
+      observer.complete();
+    });
+  }
   getQualifications(): Observable<FilterQualifications> {
     const filterqualificationsUrl = this.getBaseUrl() + 'qualifications';
     return this.http.get<FilterQualifications>(filterqualificationsUrl);
   }
-  // endpoint does not exist yet
   getQualificationTable(curriculum: number, architecturallayer: number, activity: number): Observable<QualificationsOverview[]> {
     const qualificationtableUrl = this.getBaseUrl() + 'curriculum/' + curriculum + '/architecturallayer/' + architecturallayer + '/activity/' + activity;
     return this.http.get<QualificationsOverview[]>(qualificationtableUrl);
--- src/app/models/editmodels/editable_module_output.ts
+++ src/app/models/editmodels/editable_module_output.ts
@@ -1,15 +1,19 @@
 import {TeachingMaterial} from '../teaching_material';
+import {Lecturer} from '../lecturer';
 
 export interface EditableModuleOutput {
   code: string;
   name: string;
   credits: number;
+  semester: number;
   lectures_in_week: number;
   practical_hours_week: number;
   introductorytext: string;
   topics: string[];
   teaching_material: TeachingMaterial[];
+  teaching_material_types: string[];
   additional_information: string;
-  lecturers: number[];
+  all_lecturers: Lecturer[];
+  active_lecturers: Lecturer[];
   credentials: string;
 }
