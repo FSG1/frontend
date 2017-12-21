f83beb1f63ee8d1ed1f91ecc6b4c97871814bce8
Sjoerd Brauer <s.brauer@student.fontys.nl>
2017-12-21 13:48:59 +0100

    added documentation and edited the code

--- src/app/components/skillmatrix-component/view/skillmatrix.component.ts
+++ src/app/components/skillmatrix-component/view/skillmatrix.component.ts
@@ -13,6 +13,13 @@ export class SkillMatrixComponent {
   @Input() lifecycleactivities: LifecycleActivity[];
   @Input() studentskills: StudentSkill[];
 
+  /**
+   * This method checks if there is a entry in the studentskill variable.
+   * If there is it will return the variable.
+   * @param {number} i Represents the Lifecycle activity
+   * @param {number} j represents the architectural layer
+   * @returns {number} HBO-I level
+   */
   checkForValue(i: number, j: number): number {
     for (let x = 0; x < this.studentskills.length; x++) {
       if (i === this.studentskills[x].architectural_layer && j === this.studentskills[x].lifecycle_activity) {
