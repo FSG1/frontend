59e0029c35330680ed4317586b45cb6e9fafdd0d
Tobias Derksen <t.derksen@gmx.de>
2018-01-08 12:55:34 +0100

    Changed default database ip to localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
-// WINDOWS IP:192.168.99.100
-// LINUX IP: 172.17.0.1
 
 export const environment = {
   production: false,
   backend: {
     scheme: 'http',
-    host: '172.17.0.1',
+    host: '127.0.0.1',
     port: 8080,
     base: 'fmms'
   }
