7b78ad8b44175cc69dc733ba475cefa5e1b81b78
Sjoerd Brauer <s.brauer@student.fontys.nl>
2018-01-08 13:01:06 +0100

    added more documentation

--- src/app/components/examlg-component/examlg.component.ts
+++ src/app/components/examlg-component/examlg.component.ts
@@ -2,6 +2,10 @@ import {Component, Input, OnInit} from '@angular/core';
 
 import {LearningGoal} from '../../models/learninggoal';
 
+/**
+ * This component was used at a certain point to display the exam requirements per learning goal.
+ * The customer might want to use this component again in the future
+ */
 @Component({
   selector: 'app-exam-lg',
   templateUrl: './examlg.component.html',
--- src/app/components/module-component/edit/module-edit.component.ts
+++ src/app/components/module-component/edit/module-edit.component.ts
@@ -16,6 +16,9 @@ import {SimpleModule} from '../../../models/editmodels/simple_module';
 import {AssesmentPart} from '../../../models/assesment_part';
 import {LearningGoal} from '../../../models/learninggoal';
 
+/**
+ * This component is part of the page where the module is edited
+ */
 @Component({
   selector: 'app-editable-module',
   templateUrl: './module-edit.component.html',
@@ -31,6 +34,9 @@ import {LearningGoal} from '../../../models/learninggoal';
   input: EditableModuleInput;
   modulecode: string;
 
+  /**
+   * The selected variables are variables the user changes. The name describes what it's about.
+   */
   //#region selection variables
   selectedLecturer: Lecturer;
   selectedTopic: string;
@@ -38,16 +44,37 @@ import {LearningGoal} from '../../../models/learninggoal';
   selectedTeachingMaterial: string;
   selectedTeachingMaterialType: string;
   selectedAssesmentPart: AssesmentPart;
-  selectedLearningGoal: LearningGoal;
-  defaultType = 'Teaching Material';
   selectedPriorKnowledgeRemark: string;
   selectedPriorModule: string;
-  defaultPriorModule= 'Module';
-  defaultPriorType= 'Type';
-  priorKnowledgeTypes = ['concurrent', 'mandatory', 'prior'];
+  selectedLearningGoal: LearningGoal;
   selectedpriorKnowledgeType: string;
+  /**
+   * DefaultType is the default unselected teaching material
+   * @type {string}
+   */
+  defaultType = 'Teaching Material';
+  /**
+   * This is the default unselected prior module value
+   * @type {string}
+   */
+  defaultPriorModule= 'Module';
+  /**
+   * this is the default unselected teaching material type
+   * @type {string}
+   */
+  defaultPriorType= 'Type';
+  /**
+   * This is a hardcoded list of default prior module types of which the user can choose
+   * @type {[string , string , string]}
+   */
+  priorKnowledgeTypes = ['concurrent', 'mandatory', 'prior'];
   //#endregion
 
+  /**
+   * This method loads the edit component with the module's data.
+   * It also loads the extra information required to edit the module like a list of all teachers.
+   * This method also initializes some selected variables so the corresponding buttons in the HTML are not empty
+   */
   ngOnInit(): void {
     this.selectedLearningGoal = {
       type: 'personal'
@@ -71,6 +98,10 @@ import {LearningGoal} from '../../../models/learninggoal';
             });
           }});
   }
+
+  /**
+   * In this region the methods exist to remove/select and add a lecturer to the module
+  */
   //#region lecturers
   removeLecturer(lect: Lecturer): void {
     this.output.active_lecturers = this.output.active_lecturers.filter( lec => lec.id !== lect.id);
@@ -92,6 +123,9 @@ import {LearningGoal} from '../../../models/learninggoal';
     }
   }
   //#endregion lecturers
+  /**
+   * In this region the methods exists the remove/select and add a topic to the module
+   */
   //#region topics
   removeTopic(topic: string): void {
     this.output.topics = this.output.topics.filter( top => top != topic);
@@ -127,6 +161,9 @@ import {LearningGoal} from '../../../models/learninggoal';
     }
   }
   //#endregion
+  /**
+   * In this region the methods exists the remove/select and add a teaching material to the module
+   */
   //#region teaching_materials
   removeTeachingMaterial(material: string): void {
     this.output.teaching_material = this.output.teaching_material.filter( tm => tm.name != material);
@@ -167,7 +204,10 @@ import {LearningGoal} from '../../../models/learninggoal';
     }
   }
   // endregion
-  // assesmentParts region
+  /**
+   * In this region the methods exists the remove/select and add a assesment part to the module
+   */
+  //#region assesmentParts
   canAddAssesment(): boolean {
     if (!isNullOrUndefined(this.selectedAssesmentPart.subcode) && !isNullOrUndefined(this.selectedAssesmentPart.description)  && !isNullOrUndefined(this.selectedAssesmentPart.percentage) && !isNullOrUndefined(this.selectedAssesmentPart.minimal_grade)) {
       return false;
@@ -191,6 +231,9 @@ import {LearningGoal} from '../../../models/learninggoal';
     this.output.assesment_parts = this.output.assesment_parts.filter( as => as !== assesmentPart);
   }
   //#endregion
+  /**
+   * In this region the methods exists the remove/select and add a prior knowledge reference to the module
+   */
   //#region prior knowledge references
   removePriorReference(prior: string): void {
     this.output.prior_knowledge_references = this.output.prior_knowledge_references.filter( pr => pr.name != prior);
@@ -222,6 +265,9 @@ import {LearningGoal} from '../../../models/learninggoal';
     this.selectedpriorKnowledgeType = type;
   }
   //#endregion
+  /**
+   * In this region the methods exists the remove and add a learning goal to the module
+   */
   //#region learningGoals
   canAddLearningGoal(): boolean {
     if (!isNullOrUndefined(this.selectedLearningGoal.name) && !isNullOrUndefined(this.selectedLearningGoal.description) && !isNullOrUndefined(this.selectedLearningGoal.type) && !isNullOrUndefined(this.selectedLearningGoal.weight)) {
@@ -238,7 +284,9 @@ import {LearningGoal} from '../../../models/learninggoal';
     this.selectedLearningGoal.type = type;
   }
   // #endregion
-  // send save data
+  /**
+   * This method saves the module with the inputted value
+   */
   save(): void {
     this.input = new EditableModuleInput(this.output);
     this.backendService.updateEditableModule(this.output.id, this.input).subscribe(() => {
@@ -252,12 +300,21 @@ import {LearningGoal} from '../../../models/learninggoal';
   calculateTotalEffort(): number {
     return this.output.credits * 28;
   }
+
+  /**
+   * This method checks if some mandatory fields are filled in in order to save.
+   * @returns {boolean}
+   */
   canSave(): boolean {
     if (!isNullOrUndefined(this.output.credits) && !isNullOrUndefined(this.output.lectures_in_week) && !isNullOrUndefined(this.output.practical_hours_week) && this.output.code.length > 0 && this.output.name.length > 0 && this.output.active_lecturers.length > 0 && this.output.topics.length > 0) {
       return true;
     }
     return false;
   }
+
+  /**
+   * This method takes the user back to the previous page
+   */
   back(): void {
     this.location.back();
   }
