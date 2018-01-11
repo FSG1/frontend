52edc2dfd946dfc6bd3399b0f1f6d6c5aa550167
Loek Ehren <loek-ehren@hotmail.com>
2018-01-11 15:02:54 +0100

    Fix bug where it didn't pick the right layer and activity

--- src/app/components/skillmatrix-component/edit/skillmatrix-edit.component.ts
+++ src/app/components/skillmatrix-component/edit/skillmatrix-edit.component.ts
@@ -52,7 +52,7 @@ export class SkillmatrixEditComponent implements OnInit {
     let name = '';
     if (!isNullOrUndefined(this.architecturallayers)) {
       this.architecturallayers.forEach(a => {
-        if (a.id === id) {
+        if (a.id === id + 1) {
           name = a.name;
         }
       });
@@ -69,7 +69,7 @@ export class SkillmatrixEditComponent implements OnInit {
     if (!isNullOrUndefined(this.lifecycleactivities)) {
       let name = '';
       this.lifecycleactivities.forEach(l => {
-        if (l.id === id) {
+        if (l.id === id + 1) {
           name = l.name;
         }
       });
